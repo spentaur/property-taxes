f2d8a9b76a542e794adefad60bb27b596bba5e4a
Spencer Adams <hi@spentaur.com>
2021-11-12 10:23:09 -0600

    fixing so can use in all counties

--- property-taxes.py
+++ property-taxes.py
@@ -6,20 +6,55 @@ from bs4 import BeautifulSoup
 import usaddress
 import datetime
 
+def get_payment_history(x, address, current_year):
+    return f"The taxes for {address} in {current_year} were {x['Total Billed'][0]}"
+
+def get_excemptions(x, address, current_year):
+    return f"The excemptions for {address} in {current_year} are: {x['Exemption Type'][0]}"
+
+def get_last_sale(x, address, current_year):
+    current_sale_history = x
+    last_sold_price = current_sale_history['Gross Price'][0]
+    last_sold_sale_date = current_sale_history['Sale Date'][0]
+    last_sold_sale_date = datetime.datetime.strptime(last_sold_sale_date, "%m/%d/%Y")
+    last_sold_sale_date = datetime.datetime.strftime(last_sold_sale_date, "%Y/%m/%d")
+    last_sold = f"{address} was last sold on {last_sold_sale_date} for {last_sold_price}"
+    return last_sold
 
 def get_property_tax_info(args):
+    data = {
+        "current_owner": "Unable to get current owner.",
+        "tax_rate": "Unable to get tax rate.",
+        "last_sold": "Unable to get sales data",
+        "taxes": "Unable to get taxes.",
+        "excemptions": "Unable to get excemptions."
+    }
+
+
+    columns = {
+        "['Exemption Type', 'Requested Date', 'Granted Date', 'Renewal Date', 'Prorate Date', 'Requested Amount', 'Granted Amount']": 
+            {'function': get_excemptions, 'key': 'excemptions'},
+        "['Tax Year', 'Total Billed', 'Total Paid', 'Amount Unpaid']": 
+            {'function':get_payment_history, 'key': 'taxes'},
+        "['Year', 'Document #', 'Sale Type', 'Sale Date', 'Sold By', 'Sold To', 'Gross Price', 'Personal Property', 'Net Price']":
+            {'function': get_last_sale, 'key': 'last_sold'}
+    }
+    
+    
     addr = usaddress.tag(args.address)
     house_number = addr[0]['AddressNumber']
     street_name = addr[0]['StreetName']
-    if 'StreetNamePostType' in addr[0]:
-        street_suffix = addr[0]['StreetNamePostType'].replace(".", "")
-    else:
-        street_suffix = ""
+    street_name = addr[0]['StreetName']
+    if 'StreetNamePreDirectional' in addr[0]:
+        street_name = f"{addr[0]['StreetNamePreDirectional']} {street_name}"
 
-    d = {'property_list': '', 'house_number': house_number, 'street_name': street_name, 'street_suffix': street_suffix}
+    d = {'property_list': '', 'house_number': house_number, 'street_name': street_name}
+
     r_current_year = requests.post(f"http://{args.county}il.devnetwedge.com/Search/ExecuteSearch", data=d)
 
-    last_year = str(int(r_current_year.url.split("/")[-1]) - 1)
+    current_year = int(r_current_year.url.split("/")[-1])
+
+    last_year = str(current_year - 1)
     r_last_year_url = r_current_year.url.split("/")
     r_last_year_url[-1] = last_year
     r_last_year_url = "/".join(r_last_year_url)
@@ -31,32 +66,21 @@ def get_property_tax_info(args):
     current_owner = soup.find_all('table')[0].find_all('div', {"class": "inner-value"})[2].text.split("\n")[0].split(",")
     current_owner.reverse()
     current_owner = " ".join(current_owner).strip()
+    data['current_owner'] = f"The current owner of {args.address} is {current_owner}."
 
 
     df_current = pd.read_html(r_current_year.text)
-    current_taxes = df_current[1]['Total Billed'][0]
-    current_excemptions = df_current[3]['Exemption Type'][0]
-    if 0 <= 4 < len(df_current):
-        current_sale_history = df_current[4]
-        last_sold_price = current_sale_history['Gross Price'][0]
-        last_sold_sale_date = current_sale_history['Sale Date'][0]
-        last_sold_sale_date = datetime.datetime.strptime(last_sold_sale_date, "%m/%d/%Y")
-        last_sold_sale_date = datetime.datetime.strftime(last_sold_sale_date, "%Y/%m/%d")
-        last_sold = f"{args.address} was last sold on {last_sold_sale_date} for {last_sold_price}",
-    else:
-        last_sold = f"There is no sales data for {args.address}"
-
     df_last = pd.read_html(r_last_year.text)
+
+
     tax_rate = df_last[0][1][4].replace("Tax Rate", "").strip()
+    data['tax_rate'] = f"The tax rate last year for {args.address} was {tax_rate}%."
 
+    for df in df_current:
+        if str(df.columns.tolist()) in columns:
+            x = columns[str(df.columns.tolist())]
+            data[x['key']] = x['function'](df, args.address, current_year)
 
-    data = {
-        "current_owner": f"The current owner of {args.address} is {current_owner}.",
-        "tax_rate": f"The tax rate last year for {args.address} was {tax_rate}%.",
-        "last_sold": last_sold,
-        "taxes": f"The taxes last year for {args.address} were {current_taxes}",
-        "excemptions": f"The excemptions for {args.address} are: {current_excemptions}"
-    }
 
     print(data[args.q])
 
